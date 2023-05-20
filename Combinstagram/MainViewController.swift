91097adc5461de4af9c29af7be67533a3ee92ea6
bjke <kbjwdh@gmail.com>
2023-05-20 14:28:49 +0800

    init UI

---
+++ Combinstagram/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Combinstagram
+//
+//  Created by bjke on 2023/5/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  @IBOutlet weak var imagePreview: UIImageView!
+  @IBOutlet weak var buttonClear: UIButton!
+  @IBOutlet weak var buttonSave: UIButton!
+  @IBOutlet weak var itemAdd: UIBarButtonItem!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+  @IBAction func actionClear() {
+
+  }
+
+  @IBAction func actionSave() {
+
+  }
+
+  @IBAction func actionAdd() {
+
+  }
+
+  func showMessage(_ title: String, description: String? = nil) {
+    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
+    present(alert, animated: true, completion: nil)
+  }
+
+}
+
---
+++ Combinstagram/PhotoWriter.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoWriter.swift
+//  Combinstagram
+//
+//  Created by bjke on 2023/5/20.
+//
+
+import Foundation
+
+class PhtotWriter {
+  enum Errors: Error {
+    case couldNotSavePhoto
+  }
+}
